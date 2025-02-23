8e754716f1b218d822b592a1c6523c204dab9400
JCXYOZH <2729028597@qq.com>
2025-02-23 21:56:22 +0800

    first push

---
+++ config.js
@@ -0,0 +1,14 @@
+// 应用全局配置
+module.exports = {
+	// baseURL: 'http://localhost:8160',
+	baseURL: 'http://go.free.idcfengye.com',
+	// 应用信息
+	appInfo: {
+		// 应用名称
+		name: "information-walls",
+		// 应用版本
+		version: "1.0.0",
+		// 应用logo
+		logo: "/static/logo.png",
+	}
+}
